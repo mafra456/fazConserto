1eaa81ad524988d24967958932a3d4db067a658e
mafra456 <andrecorreia.dcc@gmail.com>
2017-07-01 16:46:36 -0300

    Atualizacao com Interfaces, Classes prontas, faltando apenas finalizar a Interface de Funcionario

---
+++ src/fazConsertos/DAL/ConnectionModule.java
@@ -0,0 +1,25 @@
+package fazConsertos.DAL;
+
+import java.sql.*;
+
+public class ConnectionModule {
+
+    // Connection to MySQL db method
+    public static Connection connector() {
+        java.sql.Connection connection = null;
+        String driver = "com.mysql.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/ML";
+        String user = "root";
+        String password = "159andremafra";
+        
+        // Connecting to db
+        try {
+            Class.forName(driver);
+            connection = DriverManager.getConnection(url, user, password);
+            return connection;
+        } catch (Exception e) {
+            //System.out.println(e);
+            return null;
+        }
+    }
+}
